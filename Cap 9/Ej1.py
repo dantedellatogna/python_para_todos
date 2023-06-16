8e50e91d309d265f5153c695f22d38d202777586
dantedellatogna <dellatogna.io@gmail.com>
2023-06-16 10:08:06 -0500

    Cap 9 Ej1

---
+++ Cap 9/Ej1.py
@@ -0,0 +1,15 @@
+import random
+
+archivo = open('python_para_todos\Cap 9\words.txt')
+palabras = dict()
+
+for linea in archivo:
+    for palabra in linea.split():
+        palabras[palabra] = random.randint(0, 100)
+
+print(palabras)
+buscar_p = 'know'
+if buscar_p in palabras:
+    print(f'La palabra "{buscar_p}" está en el diccionario')
+else:
+    print(f'La palabra "{buscar_p} NO está en el diccionario')
